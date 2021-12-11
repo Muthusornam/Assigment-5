0566b3619b39d482e3fbde708c38cac4b8704ed3
Muthusornam <94225877+Muthusornam@users.noreply.github.com>
2021-12-11 11:21:55 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,9 @@
+class person{
+    constructor(name,age,address,occupation,experience){
+        this.name=name;
+        this.age=age;
+        this.address=address;
+        this.occupation=occupation;
+        this.experience=experience;
+    }
+}
