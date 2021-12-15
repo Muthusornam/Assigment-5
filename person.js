c4b6cce0ef4ab8e073c965ac12c1d3fc1e5e9f0e
Muthusornam <94225877+Muthusornam@users.noreply.github.com>
2021-12-15 19:34:41 +0530

    Delete person.js

--- person.js
+++
@@ -1,9 +0,0 @@
-class person{
-    constructor(name,age,address,occupation,experience){
-        this.name=name;
-        this.age=age;
-        this.address=address;
-        this.occupation=occupation;
-        this.experience=experience;
-    }
-}
